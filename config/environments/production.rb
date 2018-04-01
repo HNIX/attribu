b4edb86409979f5563889d394d2a4797925eb05e
Hunter Nix <hnix.au@gmail.com>
2018-04-01 14:44:03 -0500

    Add smtp settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,12 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :authentication => :plain,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["SMTP_ENTRY"],
+    :enable_starttls_auto => true
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'attribu.heroku.com' }
